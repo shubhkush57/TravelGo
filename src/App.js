002e15a517b60fc3da6ccb5e413e348773dc32a1
Shubahm <shubham.20205157@mnnit.ac.in>
2024-08-22 23:17:41 +0530

    NavBar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {CssBaseline , Grid} from '@material-ui/core'
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Placedetail from "./components/Placedetail/Placedetail";
+import Map from "./components/Map/Map";
 const App = ()=> {
   return (
-    <div>
-      Hello wordl
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing = {3} style= {{widht: '100%'}}>
+       <Grid item xs = {12} md= {4}>
+          <List />
+       </Grid>
+       <Grid item xs = {12} md= {8}>
+          <Map />
+       </Grid>
+      </Grid>
+      <Placedetail />
+    </>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import {Autocomplete} from '@react-google-maps/api';
+import SearchIcon from '@material-ui/icons/Search';
+import {AppBar, Toolbar, Typography, Box,InputBase} from '@material-ui/core';
+import useStyles from './styles';
 const Header = () => {
+ const classes = useStyles();
   return (
-    <div>Header</div>
+    <AppBar position='static'>
+        <Toolbar className = {classes.toolbar}>
+            <Typography variant='h5' className= {classes.title}>
+                Travel Go
+            </Typography>
+            <Box display='flex'>
+                <Typography variant='h6' className={classes.title}>
+                    Explore New Places
+                </Typography>
+                <Autocomplete>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon} >
+                            <SearchIcon />
+                        </div>
+                        <InputBase placeholder = "Serach new places...." classes = {{root: classes.inputRoot, input: classes.inputInput}}/>
+                    </div>
+                </Autocomplete>
+            </Box>
+        </Toolbar>
+    </AppBar>
   )
 }
 
